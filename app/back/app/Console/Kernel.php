e09272fac06c34b65369af304d214199f2906fd4
alleksandrb <bashtein@gmail.com>
2025-01-29 19:32:46 +0300

    добавил ParseInfostudCommand

--- app/back/app/Console/Kernel.php
+++ app/back/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +17,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\ParseLawsCommand::class,
         \App\Console\Commands\SaveLawsCommand::class,
         \App\Console\Commands\TranslateLawsCommand::class,
+        \App\Console\Commands\VacanciesScraping\ParseInfostudCommand::class,
     ];
 
     /**
