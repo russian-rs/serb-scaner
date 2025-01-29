ee9f4c525f3146a59ffe0e63455dd9f77a722835
alleksandrb <bashtein@gmail.com>
2025-01-29 23:21:04 +0300

    получение ссылок на все вакансии сайта в порядке "сначала новые"

--- app/back/app/Console/Commands/VacanciesScraping/ParseInfostudCommand.php
+++ app/back/app/Console/Commands/VacanciesScraping/ParseInfostudCommand.php
@@ -35,43 +35,103 @@ class ParseInfostudCommand extends Command
         try {
             $client = new Client();
 
+            $urlListVacancies = 'https://poslovi.infostud.com/oglasi-za-posao?sort=online_view_date&page=';
 
-            $response = $client->get('https://poslovi.infostud.com/oglasi-za-posao?sort=online_view_date');
-            $html = $response->getBody()->getContents();
-
-            $dom = new \DOMDocument();
-            @$dom->loadHTML($html); // Используем @, чтобы скрыть предупреждения о некорректном HTML
-
-            $xpath = new \DOMXPath($dom);
+            $pageCount = $this->getPageCount($urlListVacancies);
 
             $links = [];
-            for ($i=0; $i<100; $i++){
+            $dom = new \DOMDocument();
+            $numberVacancyOnList = 0;
 
-                $element = $xpath->query('//*[@id="oglas_'.$i.'"]');
-                if ($element->length > 0) {
+            for ($k=1; $k <= $pageCount; $k++)
+            {
+                $this->info($urlListVacancies . $k);
 
-                    // Находим ссылку на вакансию
-                    $aElem = $xpath->query('.//a', $element->item(0));
+                $response = $client->get($urlListVacancies . $k);
+                $html = $response->getBody()->getContents();
 
-                    if ($aElem->length > 0) {
-                        $href = $aElem->item(0)->getAttribute('href');
-                        $links[] = $href;
+                @$dom->loadHTML($html); // Используем @, чтобы скрыть предупреждения о некорректном HTML
+
+                $xpath = new \DOMXPath($dom);
+
+
+                while ($numberVacancyOnList < 100000){
+                    $element = $xpath->query('//*[@id="oglas_'.$numberVacancyOnList.'"]');
+                    if ($element->length > 0) {
+
+                        // Находим ссылку на вакансию
+                        $aElem = $xpath->query('.//a', $element->item(0));
+
+                        if ($aElem->length > 0) {
+                            $href = $aElem->item(0)->getAttribute('href');
+                            $links[] = $href;
+                        } else {
+                            $this->info( "Ссылка <a> не найдена внутри #oglas_$numberVacancyOnList");
+                        }
+
+                        $numberVacancyOnList++;
                     } else {
-                        echo "Ссылка <a> не найдена внутри #oglas_$i";
+                        $this->info( "Элемент с id='oglas_$numberVacancyOnList' не найден");
+                        break;
                     }
-                } else {
-                    echo "Элемент с id='oglas_0' не найден";
-                    break;
-                }
 
+                }
             }
 
-            dump($links);
+            dump(count($links));
 
             $this->info('Парсинг завершён!');
         } catch (\Exception $e) {
             $this->error('Ошибка при запросе страницы: ' . $e->getMessage());
         }
+    }
+
+    private function getPageCount(string $urlListVacancies)
+    {
+        try {
+            $client = new Client();
+            $response = $client->get($urlListVacancies);
+            $dom = new \DOMDocument();
+            $html = $response->getBody()->getContents();
+
+            @$dom->loadHTML($html); // Используем @, чтобы скрыть предупреждения о некорректном HTML
+
+            $xpath = new \DOMXPath($dom);
+            $ulElement = $xpath->query('//*[@class="uk-margin-medium-top"]//ul')->item(0);
+
+            if ($ulElement) {
+                // Находим все элементы <li> внутри <ul>
+                $liElements = $xpath->query('.//li', $ulElement);
+
+                // Проверяем, что есть хотя бы два элемента <li>
+                if ($liElements->length >= 2) {
+                    // Берем предпоследний элемент <li>
+                    $preLastLi = $liElements->item($liElements->length - 2);
+
+                    // Находим элемент <a> внутри предпоследнего <li>
+                    $aElement = $xpath->query('.//a', $preLastLi)->item(0);
+
+                    if ($aElement) {
+                        // Извлекаем текстовое значение элемента <a>
+                        $textValue = $aElement->textContent;
+                        return $textValue;
+                    } else {
+                        $this->info( "Элемент <a> не найден.");
+                    }
+                } else {
+                    $this->info( "Недостаточно элементов <li>.");
+                }
+            } else {
+                $this->info( "Элемент <ul> не найден.");
+            }
+
+
+        }catch (\Exception $e){
+            $this->error('Ошибка при запросе страницы: ' . $e->getMessage());
+        }
+
+        return 100;
+
     }
 
 
